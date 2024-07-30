421655772cae285e97d461f502a9150d25cb46a1
Axe-P <axepax72@gmail.com>
2024-07-29 23:06:33 -0600

    Added comments to code.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // TODO: Include packages needed for this application
+// Gets Inquirer package from package.json.
 import inquirer from 'inquirer';
+// Node fs (file system) is a node module that comes with node.
 import fs from 'node:fs';
+// Import generateMarkdown is grabbing the function from the .js file.
 import generateMarkdown from './utils/generateMarkdown.js';
 
 // TODO: Create an array of questions for user input
+// This array is what is displayed when ran, it prompts the user for several inputs that are later used.
 const questions = [
     {
         type: 'input',
@@ -83,25 +87,17 @@ const questions = [
             }
         }
     },
-    // {
-    //     type: 'input',
-    //     message: 'FAQ:',
-    //     name: 'questions',
-    //     validate: questionsInput => {
-    //         if (questionsInput) {
-    //             return true;
-    //         } else {
-    //             console.log('Enter any questions.');
-    //             return false;
-    //         }
-    //     }
-    // },
     {
         type: 'list',
+        // type: defines what prompt will be given to the user.
         message: 'License the project uses.',
+        // message: provides user with description of what their needing to input.
         name: 'license',
+        // name sets this "object" to be able to be called upon.
         choices: ['MIT', 'Apache', 'GPL', 'N/a'],
+        // added based off type: 'list' to give choices
         validate: licenseInput => {
+        // validates wether there was a valid input or not.
             if (licenseInput) {
                 return true;
             } else {
@@ -111,6 +107,7 @@ const questions = [
     },
     {
         type: 'input',
+        // type: input is mainly used, it is prompting the user for a string input.
         message: 'GitHub Username:',
         name: 'username',
         validate: usernameInput => {
@@ -138,7 +135,9 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
+// Using node:fs, we are creating a function that will allow us to create the README.md file. This will be called on later.
 function writeToFile(fileName, data) {
+    // fileName, sets the name of the file. Data is how it is going to be "formatted". (err) is what will be thrown if the arguments are not valid/met.
     fs.writeFile(fileName, data, (err) => {
         if (err) {
             console.error('Error writing to file:', err);
@@ -149,13 +148,19 @@ function writeToFile(fileName, data) {
 };
 
 // TODO: Create a function to initialize app
+// this function uses inquirer to pronpt the user, then it will grab the answers.
 function init() {
     inquirer.prompt(questions).then((answers) => {
+        // declaring variables that will be used to create README.md
         const markdownContent = generateMarkdown(answers);
+        // generateMarkdown function is being called in combination with the responses we recived from the inquirer.
         const readme = 'README.md';
+        // setting the type of file being created.
         writeToFile(readme, markdownContent);
+        // calling writeToFile to finally create the .md, using the answers from inqirer and setting the file type.
     })
         .catch((error) => {
+            // .catch will throw an error if there is any issue compiling the file.
             console.error('Error initializing application:', error);
         });
 };
