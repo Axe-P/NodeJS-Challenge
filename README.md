380f679bd81adcb3cc88a5a8a1e3a9c90f2e068a
Axe-P <axepax72@gmail.com>
2024-07-29 22:37:00 -0600

    Finished Markdown.js

---
+++ README.md
@@ -0,0 +1,35 @@
+# README Compiler
+
+## Description
+This uses your CLI with Node to create a README.md file.
+
+## Table of Contents
+- [Installation](#install)
+- [Tests](#test)
+- [Usage](#usage)
+- [Contributing](#contributions)
+- [License](#license)
+- [Questions](#questions)
+
+## Install
+Download files. Install node into your desired terminal. Run terminal inside directory.
+
+## Testing
+N/A
+
+## Usage
+Inside working directory with node.js run: node index.js
+
+## Contributions
+None
+
+## License
+
+## License
+This project licensed by MIT.
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+[MIT License](https://opensource.org/licenses/MIT)
+
+
+## Questions
+If you have any questions, reach out to me at [axep504@gmail.com](mailto:axep504@gmail.com)! My GitHub: [Axe-P](https://github.com/Axe-P).
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,84 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+      case 'MIT':
+          return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+      case 'Apache':
+          return '![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+      case 'GPL':
+          return '![License: GPL](https://img.shields.io/badge/License-GPL%20v3-blue.svg)';
+      default:
+          return '';
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+      case 'MIT':
+          return '[MIT License](https://opensource.org/licenses/MIT)';
+      case 'Apache':
+          return '[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)';
+      case 'GPL':
+          return '[GNU GPL v3](https://www.gnu.org/licenses/gpl-3.0)';
+      default:
+          return '';
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license || license === 'N/a') {
+      return '';
+  }
+
+  const badge = renderLicenseBadge(license);
+  const link = renderLicenseLink(license);
+
+  return `
+## License
+This project licensed by ${license}.
+${badge}
+${link}
+`;
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#install)
+- [Tests](#test)
+- [Usage](#usage)
+- [Contributing](#contributions)
+- [License](#license)
+- [Questions](#questions)
+
+## Install
+${data.install}
+
+## Testing
+${data.test}
+
+## Usage
+${data.usage}
+
+## Contributions
+${data.contributions}
+
+## License
+${renderLicenseSection(data.license)}
+
+## Questions
+If you have any questions, reach out to me at [${data.email}](mailto:${data.email})! My GitHub: [${data.username}](https://github.com/${data.username}).
 `;
-}
+};
 
 export default generateMarkdown;
